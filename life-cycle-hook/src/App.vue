95eea438f10a8e42deca554c3d7b0117fb14b83e
Himel <hasibulislam8381@gmail.com>
2024-09-06 00:35:19 +0600

    LifeCycle hook

---
+++ life-cycle-hook/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HookTest from './components/HookTest.vue'
+import { ref } from 'vue'
+const isShow = ref(true)
+</script>
+<template>
+  <div>
+    <HookTest v-if="isShow" /><br />
+    <button @click="isShow = !isShow">Show/Hide</button>
+  </div>
+</template>
---
+++ life-cycle-hook/src/components/HookTest.vue
@@ -0,0 +1,41 @@
+<script setup>
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted
+} from 'vue'
+
+const message = ref('Hello Hook Test')
+onBeforeMount(() => {
+  console.log('Component is about to Mount')
+})
+onMounted(() => {
+  console.log('Component Mounted')
+})
+onBeforeUpdate(() => {
+  console.log('Component is about to update')
+})
+
+const updateMsg = () => {
+  message.value = 'Message Updated'
+}
+onUpdated(() => {
+  console.log('Component Has been Updated')
+})
+onBeforeUnmount(() => {
+  console.log('Component is abount to unmount')
+})
+onUnmounted(() => {
+  console.log('Component Unmounted')
+})
+</script>
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <button @click="updateMsg()">change</button>
+  </div>
+</template>
