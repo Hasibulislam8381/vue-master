7507ce91c63e404c582f9d769de1a3e0cd70ef12
Himel <hasibulislam8381@gmail.com>
2024-09-11 00:14:36 +0600

    Pass

--- projects/passwordGenarator/src/components/PasswordGenerator.vue
+++ projects/passwordGenarator/src/components/PasswordGenerator.vue
@@ -1,9 +1,26 @@
 <script setup>
 import { ref } from 'vue'
 
+const passLength = ref(12)
 const isCapital = ref(true)
 const isNum = ref(true)
 const isSymbole = ref(true)
+const generatedPass = ref('')
+
+const generatePass = () => {
+  const lowChar = 'abcdefghijklmnopqrstuvwxyz'
+  const upperChar = isCapital.value ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' : ''
+  const symbol = isSymbole ? '!@#$%^&*()_+[]{}|;:,.<>?/' : ''
+  const number = isNum ? '0123456789' : ''
+
+  const allChar = lowChar + upperChar + symbol + number
+  const pass = ''
+  for (let index = 0; index < passLength; index++) {
+    const randomIndex = Math.floor(Math.random() * allChar.length)
+    pass += allChar[randomIndex]
+  }
+  generatedPass.value = pass
+}
 </script>
 <template>
   <div>
