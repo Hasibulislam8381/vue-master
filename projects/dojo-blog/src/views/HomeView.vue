d81d69e112705f701cfc76aef50b31ffa0e7fe4c
Himel <hasibulislam8381@gmail.com>
2024-09-17 23:47:01 +0600

    search

--- projects/dojo-blog/src/views/HomeView.vue
+++ projects/dojo-blog/src/views/HomeView.vue
@@ -1,8 +1,22 @@
 <script setup>
-let name = 'Himel'
-let age = 30
+import { ref, computed } from 'vue'
+let names = ref(['Himel', 'Rabu', 'Hasina', 'Majedul', 'Hamim', 'Mitu'])
+let search = ref('')
+
+// Filter
+const searchValue = computed(() => {
+  return names.value.filter((name) =>
+    name.toLocaleLowerCase().includes(search.value.toLocaleLowerCase())
+  )
+})
 </script>
 
 <template>
-  <main>Hey Hello this is {{ name }} and my age is {{ age }}</main>
+  <div class="container">
+    <input type="text" v-model="search" /><br />
+    your search key - <span style="color: brown">{{ search }}</span>
+    <div v-for="name in searchValue" :key="name">
+      {{ name }}
+    </div>
+  </div>
 </template>
