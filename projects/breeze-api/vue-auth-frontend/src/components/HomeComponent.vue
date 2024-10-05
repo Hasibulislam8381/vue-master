921f6444af541e607d5ef129ffb533c3fa791c59
Himel <hasibulislam8381@gmail.com>
2024-10-05 22:12:17 +0600

    Registration

--- projects/breeze-api/vue-auth-frontend/src/components/HomeComponent.vue
+++ projects/breeze-api/vue-auth-frontend/src/components/HomeComponent.vue
@@ -1,6 +1,15 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const user = ref()
+onMounted(async () => {
+  const data = await axios.get('/api/user')
+  console.log(data)
+})
+</script>
 <template>
   <div>
     <h1>Home</h1>
   </div>
 </template>
-<script setup></script>
